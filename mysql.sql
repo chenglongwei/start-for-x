7fa13c67d1c36c6b568f6db1bf65ae1d4fddb2be
Chenglong Wei <weichenglong@gmail.com>
2016-07-15 22:33:55 -0700

    add APIs

---
+++ mysql.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE start_for_x;
+
+CREATE TABLE `start_for_x`.`startup_tag` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `tag` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
--- start-for-x.py
+++ start-for-x.py
@@ -1,11 +1,41 @@
 from flask import Flask, request, render_template
+from flaskext.mysql import MySQL
 
+mysql = MySQL()
 app = Flask(__name__)
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'start_for_x'
+
+mysql.init_app(app)
 
 
 @app.route('/')
 def hello_world():
     return render_template('home.html')
+
+
+@app.route('/api/create_startup', methods=['POST'])
+def create_startup():
+    return "OK"
+
+
+@app.route('/api/finances/', methods=['GET'])
+def get_finances():
+    return "OK"
+
+
+@app.route('/api/news/', methods=['GET'])
+def get_news():
+    return "OK"
+
+
+@app.route('/api/workplace/', methods=['GET'])
+def get_workplace():
+    return "OK"
 
 
 if __name__ == '__main__':
