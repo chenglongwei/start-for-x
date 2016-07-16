5f96fd6b73400f11a20444c4f392e246f9705efc
Chenglong Wei <weichenglong@gmail.com>
2016-07-16 01:32:42 -0700

    api news

--- mysql.sql
+++ mysql.sql
@@ -1,7 +1,41 @@
 CREATE DATABASE start_for_x;
 
+
 CREATE TABLE `start_for_x`.`startup_tag` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `startup_name` VARCHAR(45) NULL,
   `tag` VARCHAR(128) NULL,
+  PRIMARY KEY (`id`));
+
+
+CREATE TABLE `start_for_x`.`startup_news` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `startup_name` VARCHAR(45) NULL,
+  `link` VARCHAR(1024) NULL,
+  PRIMARY KEY (`id`));
+
+
+CREATE TABLE `start_for_x`.`employee` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(64) NULL,
+  `location` VARCHAR(256) NULL,
+  `work_history` VARCHAR(4096) NULL,
+  `startup_name` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
+
+
+CREATE TABLE `start_for_x`.`startup_evaluation` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `startup_name` VARCHAR(45) NULL,
+  `current_valuation` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
+
+
+CREATE TABLE `start_for_x`.`startup_funding` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `date` DATETIME NULL,
+  `event` VARCHAR(45) NULL,
+  `amount_raised` VARCHAR(45) NULL,
+  `investors` VARCHAR(2048) NULL,
+  `startup_name` VARCHAR(45) NULL,
   PRIMARY KEY (`id`));
--- start-for-x.py
+++ start-for-x.py
@@ -30,7 +30,8 @@ def create_startup():
         cursor = conn.cursor()
 
         for tag in data['tags']:
-            cursor.execute('''INSERT INTO startup_tag (startup_name, tag) VALUES (%s, %s)''', (data['startup_name'], tag))
+            cursor.execute('''INSERT INTO startup_tag (startup_name, tag) VALUES (%s, %s)''',
+                           (data['startup_name'], tag))
             conn.commit()
 
         js = json.dumps({'status': 'OK'})
@@ -47,12 +48,57 @@ def get_finances(name):
 
 @app.route('/api/news/<name>', methods=['GET'])
 def get_news(name):
-    return "OK"
+    if request.method == 'GET':
+        tags = get_company_tags(name)
+        companies = get_finances_helper(tags)
+
+        conn = mysql.connect()
+        cursor = conn.cursor()
+
+        res = []
+        for company in companies:
+            cursor.execute('''SELECT link FROM startup_news WHERE startup_name = %s''', (company,))
+            conn.commit()
+            links = cursor.fetchall()
+            sub_res = {company: links}
+            res.append(sub_res)
+
+        js = json.dumps(res)
+        print js
+        resp = Response(js, status=200, mimetype="application/jsonp")
+        resp.headers.add('Access-Control-Allow-Origin', '*')
+        return resp
 
 
 @app.route('/api/workplace/<name>', methods=['GET'])
 def get_workplace(name):
     return "OK"
+
+
+def get_company_tags(startup_name):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute('''SELECT tag FROM startup_tag WHERE startup_name = %s''', (startup_name,))
+    conn.commit()
+
+    tags = cursor.fetchall()
+
+    return tags
+
+
+def get_finances_helper(tags):
+    print tags
+    return ['company1', 'company2', 'company3', 'start_for_x']
+
+
+def get_news_helper(tags):
+    print tags
+    return ['company1', 'company2', 'company3', 'start_for_x']
+
+
+def get_workplace_helper(tags):
+    print tags
+    return ['company1', 'company2', 'company3', 'start_for_x']
 
 
 if __name__ == '__main__':
